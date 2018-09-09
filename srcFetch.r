546a37399e728110a65b1625416a91ae99de5386
Hugo Day <hugoaday@gmail.com>
2018-09-09 13:13:58 +0100

    Script to fetch test data

---
+++ srcFetch.r
@@ -0,0 +1,9 @@
+### File to check existence of text data set or if not present, download to directory.
+### Dataset sourced from https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip
+
+setwd("F:/JohnHopkinsDataScienceCapstoneProject/")
+
+if(!file.exists("./dataset/Coursera-SwiftKey.zip")) {
+  dir.create("./dataset/")
+  download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip","./dataset/Coursera-SwiftKey.zip")
+}
